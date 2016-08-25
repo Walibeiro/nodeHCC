b71eb97299c99f8f166e5f8e2ffd4d1f7e6be83c
Walibeiro <Walison@pcjv.de>
2016-08-25 17:49:08 +0200

    Update install script

--- install.js
+++ install.js
@@ -2,3 +2,27 @@
 
 'use strict'
 
+var https = require('https');
+var path = require('path');
+var fs = require('fs');
+
+var binPath = path.join(__dirname, 'bin');
+var binLocation = path.join(binPath, 'HCC.exe');
+
+var mkdirSync = function (path) {
+  try {
+    fs.mkdirSync(path);
+  } catch(e) {
+    if ( e.code != 'EEXIST' ) throw e;
+  }
+}
+
+mkdirSync(binPath);
+var file = fs.createWriteStream(binLocation);                                                           
+var request = https.get("https://raw.githubusercontent.com/Walibeiro/nodeHCC/master/Binaries/x86/HCC.exe", function(response) {
+  console.log('Download binary command-line compiler');
+  response.pipe(file);                       
+  file.on('finish', function() {
+    console.log('Done');
+  });                                  
+});
